6d695ffcec1c0db190f06f7de773ce5b5fb06c20
user <vladimirgailov@gmail.com>
2023-04-24 13:01:45 +0300

    2 commit

---
+++ CoffeHouse3914/ClassHelper/Stuff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeHouse3914.DB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Core.Common.CommandTrees;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+    public partial class Stuff
+    {
+        public int Quantity { get; set; }
+       
+    }
+}
---
+++ CoffeHouse3914/Windows/CartWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using CoffeHouse3914.Windows;
+using CoffeHouse3914.DB;
+using CoffeHouse3914.ClassHelper;
+
+namespace CoffeHouse3914.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для CartWindow.xaml
+    /// </summary>
+    public partial class CartWindow : Window
+    {
+        public CartWindow()
+        {
+            InitializeComponent();
+            Getlistproduct();
+        }
+        private void Getlistproduct()
+        {
+            ObservableCollection<DB.Stuff> stuffs= new ObservableCollection<DB.Stuff>(ClassHelper.CartClass.stuffs);
+
+            LvCartList.ItemsSource= stuffs;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ToCart_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+            var selectedProduct = button.DataContext as DB.Stuff;
+            if (selectedProduct != null)
+            {
+                ClassHelper.CartClass.stuffs.Remove(selectedProduct);
+                Getlistproduct();
+            }
+        }
+
+        private void PlusProduct_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+            var selectedProduct = button.DataContext as DB.Stuff;
+                selectedProduct.Quantity++;
+                Getlistproduct();
+        }
+
+        private void MinusProduct_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+            var selectedProduct = button.DataContext as DB.Stuff;
+                selectedProduct.Quantity--;
+                Getlistproduct();
+        }
+
+        
+        private void CreateOrder_Click(object sender, RoutedEventArgs e)
+        {
+          
+            Check check = new Check();
+            check.IDEmployee = ClassHelper.UserDataClass.Emploee.IDEmploee;
+            check.IDGuest = 1;
+            check.Date = DateTime.Now;
+            if (check!=null)
+            {
+                EFClass.Context.Check.Add(check);
+                EFClass.Context.SaveChanges();
+            }
+            foreach (var item in CartClass.stuffs)
+            {
+                StuffList stuffList = new StuffList();
+                stuffList.IDStuff = item.IDStuff;
+                stuffList.IDCheck = EFClass.Context.Check.ToList().LastOrDefault().IDCheck;
+                stuffList.Quantity= item.Quantity;
+
+                EFClass.Context.StuffList.Add(stuffList);
+                EFClass.Context.SaveChanges();
+                MessageBox.Show("Заказ успешно создан");
+
+            }
+
+        }
+    }
+}
